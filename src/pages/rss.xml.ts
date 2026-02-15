5d464803507a1c5f3004012798f228f9056f1771
enkhee-Osiris <enkhee.ag@gmail.com>
2026-02-15 20:56:48 +0800

    update: js to ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,20 @@
 import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts";
 
-export async function GET(context) {
+export const GET: APIRoute = async context => {
   const posts = await getCollection("blog");
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    site: context.site!,
     items: posts.map(post => ({
       ...post.data,
       link: `/blog/${post.id}/`,
     })),
   });
-}
+};
